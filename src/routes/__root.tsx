b8528caeb9f5094984b539205cb49e3389c3d444
Md Shaon Sikder <shaonsikder1971@gmai.com>
2026-06-28 12:50:17 +0200

    feat: install Microsoft Clarity tracking script

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -124,6 +124,17 @@ function RootShell({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "xe2avo3ibb");
+            `,
+          }}
+        />
       </head>
       <body>
         {children}
